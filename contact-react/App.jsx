a12d3c725e6daef82ab76259059363f75c801895
Fjolla12 <fjolla.jakupi@student.uni-pr.edu>
2026-01-03 11:47:01 +0100

    commit

---
+++ contact-react/App.jsx
@@ -0,0 +1,14 @@
+//React Project: Contact page
+//UI library: React Bootstrap
+//Only Contact page is implanted
+
+import{useState}from "react";
+import { Container,Form, Button, Alert } from "react-bootstrap";
+
+function App(){
+    const[name,setname]=useState("");const[email,setEmail]=useState("");
+    const[message,setMessage]=useState("");
+    const[error,setError]=useState("");const[success,setSuccess]=useState(false);
+
+    const handle
+};
